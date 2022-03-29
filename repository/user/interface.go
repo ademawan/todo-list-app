461de1a66cd6b0bcfab643875e6d2ce06f59488b
Ade Mawan <ademawan1210@gmail.com>
2022-03-29 20:14:20 +0700

    create route for user

--- repository/user/interface.go
+++ repository/user/interface.go
@@ -7,5 +7,6 @@ type User interface {
 	GetByUid(userUid string) (entities.User, error)
 	Update(userUid string, newUser entities.User) (entities.User, error)
 	Delete(userUid string) error
+	//jika diperlukan
 	// GetAll() ([]entities.User, error)
 }
