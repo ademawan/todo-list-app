ef6212d349f6c57b777fff0fe3aba16194b81d62
Ade Mawan <ademawan1210@gmail.com>
2022-03-29 19:39:10 +0700

    add entities

--- entities/task.go
+++ entities/task.go
@@ -1,12 +1,22 @@
 package entities
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Task struct {
-	gorm.Model
-	Nama     string
-	Priority int
-	User_ID  int `gorm:"index;column:user_id" json:"user_id"`
-	Status   int
-	Category string
+	TaskUid  string `gorm:"index;unique;type:varchar(22)" json:"task_uid"`
+	UserUid  string `gorm:"index;type:varchar(22)" json:"user_uid"`
+	Title    string `gorm:"type:varchar(30)" json:"title"`
+	Priority string `gorm:"type:enum('hight','medium','low')" json:"gender"`
+	User_ID  int    `gorm:"index;column:user_id" json:"user_id"`
+	Status   int    `gorm:"type:enum('done','undone','ignore')" json:"status"`
+	Note     string `gorm:"type:varchar(250)" json:"note"`
+
+	CreatedAt      time.Time      `json:"-"`
+	UpdatedAt      time.Time      `json:"-"`
+	DeletedAt      gorm.DeletedAt `gorm:"index" json:"-"`
+	Todo_date_time time.Time      `gorm:"index" json:"todo_date_time"`
 }
--- entities/user.go
+++ entities/user.go
@@ -1,14 +1,22 @@
 package entities
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 type User struct {
-	gorm.Model
-	Nama     string `gorm:"index"`
-	Email    string
-	Password string
+	UserUid  string `gorm:"index;unique;type:varchar(22)" json:"user_uid"`
+	Name     string `gorm:"type:varchar(30)" json:"name"`
+	Email    string `gorm:"unique" json:"email"`
+	Password string `gorm:"-" json:"-"`
+	Address  string `gorm:"type:varchar(200)" json:"address"`
+	Gender   string `gorm:"type:enum('male','female')" json:"gender"`
 
-	Task []Task `gorm:"ForeignKey:User_ID"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `gorm:"-" json:"-"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+
+	Task []Task `gorm:"ForeignKey:UserUid"`
 }
